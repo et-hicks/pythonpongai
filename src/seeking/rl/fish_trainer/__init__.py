d0043157c19afb093eece1cb62ebf6d08b2f391d
et-hicks <ehicks@berkeley.edu>
2025-12-02 00:44:59 -0500

    unknown state, unknown commit

--- src/seeking/rl/fish_trainer/__init__.py
+++ src/seeking/rl/fish_trainer/__init__.py
@@ -1,12 +1,14 @@
 """Controllers for the ASCII fish game."""
 
-from .fish_dqn import FishDQN
+from .fish_dqn import FishDQN, ACTION_SPACE, Transition
 from .fish_ac import FishActorCritic
 from .fish_regression import FishRegressionController
 from .state import quantize_ascii_frame, QuantizedFishState
 
 __all__ = [
     "FishDQN",
+    "ACTION_SPACE",
+    "Transition",
     "FishActorCritic",
     "FishRegressionController",
     "quantize_ascii_frame",
